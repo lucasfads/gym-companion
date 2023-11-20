a1f957d83bebdd6f415f80e0dfb2241a0ed0af0c
Lucas Fads <lucasfads@gmail.com>
2023-11-19 21:05:40 -0300

    chore: can import workouts component

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,13 @@
 import { useState } from 'react'
 import './App.css'
 
+import WorkoutsList from './components/workouts'
+
 function App() {
 
   return (
     <>
+      <WorkoutsList></WorkoutsList>
       <div>Hello World</div>
     </>
   )
---
+++ src/components/workouts/index.tsx
@@ -0,0 +1,13 @@
+import styles from './styles.module.css';
+
+
+const WorkoutsList: React.FC = () => {
+	
+	return (
+		<div className={`${styles.workouts}`}>
+		  Hello Workouts
+		</div>
+	);
+
+}
+export default WorkoutsList;
