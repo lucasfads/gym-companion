e37faba8aedb8fd7d2d254d7aae951bd31095ea0
Lucas Fads <lucasfads@gmail.com>
2023-11-20 13:44:34 -0300

    refactor: rename workouts-list path

--- src/App.tsx
+++ src/App.tsx
@@ -2,13 +2,14 @@ import { useState } from 'react'
 import { Routes, Route, Outlet, Link } from "react-router-dom";
 import './App.css'
 
-import WorkoutsList from '@/src/components/workouts'
+import WorkoutsList from '@/src/components/workouts-list'
 
 function App() {
 
   return (
     <Routes>
       <Route path="/" element={<WorkoutsList />} />
+      {/* <Route path="/workout/:id" element={WorkoutDetails} /> */}
     </Routes>
   );
 }
