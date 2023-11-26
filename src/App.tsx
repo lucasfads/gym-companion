14c453dbecb7eae342285073598c1f35df4dc971
Lucas Fads <lucasfads@gmail.com>
2023-11-26 11:14:40 -0300

    chore: moved route elements to new pages folder

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,9 @@
 import { Routes, Route, Outlet, Link } from "react-router-dom";
 import './App.css'
 
-import WorkoutsList from '@/src/components/workouts-list';
-import WorkoutDetails from '@/src/components/workout-details';
-import ProgramDetails from '@/src/components/program-details'
+import WorkoutsList from '@/src/pages/workouts-list';
+import WorkoutDetails from '@/src/pages/workout-details';
+import ProgramDetails from '@/src/pages/program-details'
 
 function App() {
 
