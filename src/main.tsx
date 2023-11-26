e533aee8ac9dacff16b7d5be8e2ae7974926386e
Lucas Fads <lucasfads@gmail.com>
2023-11-26 15:57:30 -0300

    fix: base path changed

--- src/main.tsx
+++ src/main.tsx
@@ -6,7 +6,7 @@ import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename='/gym-companion'>
       <App />
     </BrowserRouter>
   </React.StrictMode>,
